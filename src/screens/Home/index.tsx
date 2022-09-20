cc5687642019fd2cdd70679fafb55d1b71005724
Filipi Rafael <filipirafael.123@gmail.com>
2022-09-20 18:30:40 -0300

    Add new updates on mobile app

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -16,8 +16,8 @@ export function Home() {
 
   const navigation = useNavigation();
 
-  const handleOpenGame = () => {
-    navigation.navigate('game');
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
   };
 
   useEffect(() => {
@@ -46,7 +46,7 @@ export function Home() {
             renderItem={({ item }) => (
               <GameCard
                 data={item}
-                onPress={handleOpenGame}
+                onPress={() => handleOpenGame(item)}
               />
             )}
             horizontal
